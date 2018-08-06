c102dda3e18696af7e409f8cb2861a0b6a68ba2f
m.alekseev <alekseevmatvej@gmail.com>
2018-08-06 10:08:33 +0300

    db

--- msg.py
+++ msg.py
@@ -8,6 +8,7 @@ not_private_msg = 'Только для личных чатов'
 sbor_complete = 'Сбор отправлен'
 sbor_complete_md = '\n*Сбор отправлен:* '
 count = '\n*Сообщений отправил:* '
+error_count = '\n*Ошибок*: '
 empty_send_list = '\nСписок для отправки пуст'
 
 btn_static = 'Статистика'
