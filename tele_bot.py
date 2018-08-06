a69d7be8c40bfeccc881956f938f2bbec1ecf243
m.alekseev <alekseevmatvej@gmail.com>
2018-08-06 16:37:53 +0300

    db

--- tele_bot.py
+++ tele_bot.py
@@ -564,7 +564,7 @@ except:
 
 def job_time():
     for chat in select("select chat_id from chats where status = 0;"):
-        bot.send_message(chat[0], msg_time_jira, parse_mode='MARKDOWN')
+        bot.send_message(chat[0], msg_time_jira, parse_mode='MARKDOWN', disable_web_page_preview=True)
 
 
 def job_bd():
@@ -577,11 +577,11 @@ def job_bd():
             if birthday[3:] == str(current_month) or birthday[3:] == '0'+str(current_month):
                 text = text + birthday[:2] + " - [" + bd[1] + "](https://t.me/" + bd[2] + ")\n"
         for chat in select("select chat_id from chats where status = 0;"):
-            bot.send_message(chat[0], db_label + text, parse_mode='MARKDOWN')
+            bot.send_message(chat[0], db_label + text, parse_mode='MARKDOWN',disable_web_page_preview=True)
 
 
-schedule.every().day.at("15:34").do(job_time)
-schedule.every().day.at("15:34").do(job_bd)
+schedule.every().day.at("15:39").do(job_time)
+schedule.every().day.at("15:39").do(job_bd)
 
 while True:
     schedule.run_pending()
