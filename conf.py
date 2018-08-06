dc50083b4623cc83691acc8108551fc708740708
m.alekseev <alekseevmatvej@gmail.com>
2018-08-06 17:06:49 +0300

    db

--- conf.py
+++ conf.py
@@ -1,5 +1,7 @@
 telegrambot_test = "575202317:AAGlHvAyBjPlBlOS0oxLOB6toarGV51HxVQ"
 db = "fmp_database.db"
-bd_sched_time = "15:55"
-jira_sched_time = "15:55"
+bd_sched_time = "09:00"
+bd_sched_day = [1, 7]
+jira_sched_time = "08:30"
+jira_sched_day = [1, 20, 7]
 #telegrambot_elem = "420685937:AAGQPM_E6O6HNBjv90u7TSMIxUJzAxO-P0o"
--- scheduler.py
+++ scheduler.py
@@ -7,19 +7,24 @@ import time
 import schedule
 
 def job_time():
-    for chat in select("select chat_id from chats where status = 0;"):
-        bot.send_message(chat[0], msg_time_jira, parse_mode='MARKDOWN', disable_web_page_preview=True)
+    current_day = datetime.now().day
+    if current_day in jira_sched_day:
+        for chat in select("select chat_id from chats where status = 0;"):
+            bot.send_message(chat[0], msg_time_jira, parse_mode='MARKDOWN', disable_web_page_preview=True)
 
 
 def job_bd():
     current_day = datetime.now().day
     current_month = datetime.now().month
-    if current_day == 6:
+    if current_day in bd_sched_day:
         text = ""
         for bd in select("select birthday,name,username from chats where status = 0;"):
             birthday = bd[0]
             if birthday[3:] == str(current_month) or birthday[3:] == '0'+str(current_month):
-                text = text + birthday[:2] + " - [" + bd[1] + "](https://t.me/" + bd[2] + ")\n"
+                if bd[2] != 'None':
+                    text = text + birthday[:2] + " - [" + bd[1] + "](https://t.me/" + bd[2] + ")\n"
+                else:
+                    text = text + birthday[:2] + " - " + bd[1] + "\n"
         for chat in select("select chat_id from chats where status = 0;"):
             bot.send_message(chat[0], db_label + text, parse_mode='MARKDOWN',disable_web_page_preview=True)
 
