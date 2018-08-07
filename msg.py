363b1aa0ed118d7ddd3e0939ef667f9abc5e7fce
m.alekseev <alekseevmatvej@gmail.com>
2018-08-07 09:20:55 +0300

    db

--- msg.py
+++ msg.py
@@ -21,6 +21,77 @@ label_status4 = '*Другие*\n'
 label_pay_1 = 'Пользователь '
 label_pay_2 = ' говорит, что перевёл, надо бы подтвердить'
 
+btn_url1 = 'Регламенты'
+label_url1 = 'Здесь можно найти полезные регламенты'
+url_1 = []
+url = []
+url.append('Списание рабочего времени')
+url.append('https://conf.bars-open.ru/pages/viewpage.action?pageId=28403312')
+url_1.append(url)
+url = []
+url.append('Списание нерабочего времени')
+url.append('https://conf.bars-open.ru/pages/viewpage.action?pageId=44600064')
+url_1.append(url)
+url = []
+url.append('Согласование технологических работ')
+url.append('https://conf.bars-open.ru/pages/viewpage.action?pageId=32049678')
+url_1.append(url)
+url = []
+url.append('Постановка задач')
+url.append('https://conf.bars-open.ru/pages/viewpage.action?pageId=30413239')
+url_1.append(url)
+url = []
+url.append('Предостувление доступа к БД')
+url.append('https://conf.bars-open.ru/pages/viewpage.action?pageId=30408754')
+url_1.append(url)
+
+btn_url2 = 'ЕГИСЗ'
+label_url2 = 'Здесь можно найти полезные знания по ЕГИСЗ'
+url_2 = []
+url = []
+url.append('Глоссарий')
+url.append('https://conf.bars-open.ru/pages/viewpage.action?pageId=15283407')
+url_2.append(url)
+url = []
+url.append('Реестр систем')
+url.append('https://conf.bars-open.ru/pages/viewpage.action?pageId=31615062')
+url_2.append(url)
+url = []
+url.append('Ответственные по системам')
+url.append('https://conf.bars-open.ru/pages/viewpage.action?pageId=49599846')
+url_2.append(url)
+
+btn_url3 = 'Ресурсы'
+label_url3 = 'Здесь можно найти ссылки на полезные ресурсы для комфортной работы'
+url_3 = []
+url = []
+url.append('Портал БАРС Груп')
+url.append('https://portal.bars-open.ru/')
+url_3.append(url)
+url = []
+url.append('Confluence')
+url.append('https://conf.bars-open.ru/')
+url_3.append(url)
+url = []
+url.append('Jira')
+url.append('http://jira.bars-open.ru/')
+url_3.append(url)
+url = []
+url.append('Личный кабинет')
+url.append('https://lk.bars-open.ru/user/login')
+url_3.append(url)
+url = []
+url.append('Бронирование переговорных')
+url.append('https://portal.bars-open.ru/services/index.php')
+url_3.append(url)
+url = []
+url.append('Заказ техники')
+url.append('https://portal.bars-open.ru/services/bp/121/list.php')
+url_3.append(url)
+url = []
+url.append('Реестр бланков заявлений')
+url.append('https://conf.bars-open.ru/pages/viewpage.action?pageId=16452933')
+url_3.append(url)
 
 btn_confirm = 'Подтвердить'
 btn_status1 = 'Не перевели'
@@ -54,7 +125,7 @@ btn_kid = 'Рождение ребёнка'
 btn_bye = 'Увольнение'
 
 
-msg_time_jira = 'Не забудь списать время в [Jira](https://jira.bars-open.ru)'
+msg_time_jira = 'Не забудь списать время в [Jira](' + url_jira + ')'
 msg_menu = 'А вот и меню'
 msg_confirm = 'Ваш перевод потверждён'
 msg_thank_admin = 'Спасибо'
