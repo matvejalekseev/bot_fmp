860e7cf1573a7218a091f371ee9d68a937686733
m.alekseev <alekseevmatvej@gmail.com>
2018-08-08 18:05:16 +0300

    db

--- db.py
+++ db.py
@@ -5,43 +5,43 @@ conn = sqlite3.connect(db) # или :memory: чтобы сохранить в RA
 cursor = conn.cursor()
  
 # Создание таблицы "Чаты"
-cursor.execute("DROP TABLE chats;")
-cursor.execute("CREATE TABLE chats(chat_id real, username text DEFAULT 'None', name text DEFAULT 'Your name', status INTEGER DEFAULT 0, UNIQUE(chat_id) );")
+#cursor.execute("DROP TABLE chats;")
+#cursor.execute("CREATE TABLE chats(chat_id real, username text DEFAULT 'None', name text DEFAULT 'Your name', status INTEGER DEFAULT 0, UNIQUE(chat_id) );")
 #моя личка chat_id = '109099327'
-cursor.execute("insert into chats(status,chat_id) values (1,-241874218);")
-cursor.execute("ALTER TABLE chats add birthday text;")
+#cursor.execute("insert into chats(status,chat_id) values (1,-241874218);")
+#cursor.execute("ALTER TABLE chats add birthday text;")
 
 # Создание таблицы "Статистики"
-cursor.execute("DROP TABLE stats;")
-cursor.execute("CREATE TABLE stats(stat text, number INTEGER DEFAULT 0, name text,  UNIQUE(stat) );")
+#cursor.execute("DROP TABLE stats;")
+#cursor.execute("CREATE TABLE stats(stat text, number INTEGER DEFAULT 0, name text,  UNIQUE(stat) );")
 #cursor.execute("INSERT INTO stats(stat,name) VALUES ('likes','Лайков');")
 #cursor.execute("INSERT INTO stats(stat,name) VALUES ('dislikes','Дизлайков');")
-cursor.execute("INSERT INTO stats(stat,name) VALUES ('messages','Всего получено сообщений');")
+#cursor.execute("INSERT INTO stats(stat,name) VALUES ('messages','Всего получено сообщений');")
 #cursor.execute("INSERT INTO stats(stat,name) VALUES ('mass_messages','Всего отправлено сообщений в рассылках');")
 #cursor.execute("INSERT INTO stats(stat,name) VALUES ('orders_send','Всего отправлено предзаказов');")
 
 
 # Создание таблицы "Мероприятия"
-cursor.execute("DROP TABLE events;")
-cursor.execute("CREATE TABLE events(id integer primary key autoincrement, chat_id real, status INTEGER DEFAULT 0, "
-               "name text DEFAULT 'None', price text DEFAULT 'None', account text DEFAULT 'None');")
+#cursor.execute("DROP TABLE events;")
+#cursor.execute("CREATE TABLE events(id integer primary key autoincrement, chat_id real, status INTEGER DEFAULT 0, "
+#               "name text DEFAULT 'None', price text DEFAULT 'None', account text DEFAULT 'None');")
 
 # Создание таблицы связи "Виновники меровприятий"
-cursor.execute("DROP TABLE u2e;")
-cursor.execute("CREATE TABLE u2e(event_id integer, chat_id real, UNIQUE(chat_id,event_id));")
+#cursor.execute("DROP TABLE u2e;")
+#cursor.execute("CREATE TABLE u2e(event_id integer, chat_id real, UNIQUE(chat_id,event_id));")
 
 # Создание таблицы "Статус текущего сбора"
-cursor.execute("DROP TABLE status_sbor;")
+#cursor.execute("DROP TABLE status_sbor;")
 #10 - новый
 #0 - не сдал
 #1 - сдал, но не подтведил
 #2 - сдал и подтвердил
-cursor.execute("CREATE TABLE status_sbor(chat_id real, status integer default 10, UNIQUE(chat_id));")
+#cursor.execute("CREATE TABLE status_sbor(chat_id real, status integer default 10, UNIQUE(chat_id));")
 
 # Создание таблицы "Инвайт"
-cursor.execute("DROP TABLE invite;")
-cursor.execute("CREATE TABLE invite(str text);")
-cursor.execute("insert into invite(str) values ('hello');")
+#cursor.execute("DROP TABLE invite;")
+#cursor.execute("CREATE TABLE invite(str text);")
+#cursor.execute("insert into invite(str) values ('hello');")
 
 conn.commit()
 conn.close()
--- test.py
+++ test.py
@@ -1,17 +1,6 @@
-from functions import *
-from random import choice
-from string import ascii_uppercase
+data = []
 
-
-
-def inchats(s):
-    chats = select("select chat_id from chats where chat_id = " + str(s) + ";")
-    if chats:
-        return True
-    else:
-        return False
-
-if inchats(-241874218):
-    print('1')
-else:
-    print('0')
+data.append([0, 1])
+data.append([0,2])
+for row in data:
+    print(row)
