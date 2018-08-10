c9b470a0f602fa048f8220e2bc2f05667abe79a2
m.alekseev <alekseevmatvej@gmail.com>
2018-08-10 10:20:39 +0300

    db

--- tele_bot.py
+++ tele_bot.py
@@ -289,7 +289,7 @@ def echo_message(message):
                 for user in users:
                     text = text + str(user[1]) + " " + str(user[3]) + " " \
                            + prettyUsername_wA(str(user[0])) + " " + str(round(user[2])) + "\n"
-                    if len(text) > 200:
+                    if len(text) > 150:
                         bot.send_message(chat_id, ladel_users + text, parse_mode='MARKDOWN')
                         text = ""
                 if len(text) > 0:
